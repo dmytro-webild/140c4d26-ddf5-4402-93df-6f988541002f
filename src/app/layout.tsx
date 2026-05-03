31bfc65260be5917a401aa7324ad7604a326968c
dk <kudin.dima@gmail.com>
2026-05-03 09:06:01 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { Raleway } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'Al Posto Giusto | Ristorante di Mare Nola',
+  description: 'Autentica cucina di mare nel cuore di Nola. Tradizione familiare, pesce fresco e sapori indimenticabili.',
+};
+
+const raleway = Raleway({
+  variable: "--font-raleway",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${raleway.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
